8a5c00ef43fcaf88976fbefa5e175622ae40e003
StanMarek <stanislawmarek21@gmail.com>
2021-11-11 09:49:52 +0100

    TODO: calc matrix H

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from model.element import Element
-from model.element4_2D import Element4_2D, jacobian
+from model.element4_2D import Element4_2D, calcHMatrix, jacobian
 from model.grid import Grid
 from model.node import Node
 from util.function import gauss_1dim, gauss_2dim
@@ -11,9 +11,12 @@ def main():
 	el = Element4_2D(4)
 	el.display()
 
-	for i in range(grid.nE):
-		for j in range(el.nPoints):
-			jacobian(i, j, el, grid)
+	
+	for elementNumber in range(grid.nE):
+		for integrationPoint in range(el.nPoints):
+			jac = jacobian(elementNumber, integrationPoint, el, grid)
+
+	calcHMatrix(el, jac, 0, 0)
 
 if __name__ == '__main__':
 	main()
--- src/model/element4_2D.py
+++ src/model/element4_2D.py
@@ -26,11 +26,14 @@ class Element4_2D:
         self.matrix_dNdEta[i][j] = dNdEta(ksi[i])[j]
     
   def display(self):
-    print('Matrix dN/dKsi\n', np.matrix(self.matrix_dNdKsi))
-    print('Matrix dN/dEta\n', np.matrix(self.matrix_dNdEta))
+    print('Matrix dN/dKsi')
+    printMatrix(self.matrix_dNdKsi)
+    print('Matrix dN/dEta')
+    printMatrix(self.matrix_dNdEta)
 
 
 def printMatrix(matrix):
+  np.set_printoptions(precision=5)
   print(np.matrix(matrix))
 
 
@@ -38,18 +41,19 @@ def jacobian(nOfElem, nOfIP, e42d: Element4_2D, grid: Grid):
   dXdKsi = dYdEta = dXdEta = dYdKsi = 0.0
 
   for j in range(4):
-    # dXdKsi += e42d.matrix_dNdKsi[nOfIP][j] * grid.nodes[grid.elements[nOfElem].ID[j] -1].x
-    # dYdEta += e42d.matrix_dNdEta[nOfIP][j] * grid.nodes[grid.elements[nOfElem].ID[j] -1].y
-    # dXdEta += e42d.matrix_dNdEta[nOfIP][j] * grid.nodes[grid.elements[nOfElem].ID[j] -1].x
-    # dYdKsi += e42d.matrix_dNdEta[nOfIP][j] * grid.nodes[grid.elements[nOfElem].ID[j] -1].y
+    # nodeId = grid.elements[nOfElem].ID[j] - 1
+    # dXdKsi += e42d.matrix_dNdKsi[nOfIP][j] * grid.nodes[nodeId].x
+    # dYdEta += e42d.matrix_dNdEta[nOfIP][j] * grid.nodes[nodeId].y
+    # dXdEta += e42d.matrix_dNdEta[nOfIP][j] * grid.nodes[nodeId].x
+    # dYdKsi += e42d.matrix_dNdKsi[nOfIP][j] * grid.nodes[nodeId].y
     dXdKsi += X[j] * e42d.matrix_dNdKsi[nOfIP][j] 
     dYdEta += Y[j] * e42d.matrix_dNdEta[nOfIP][j]
 
   J = transposeJ = [[0 for y in range(2)] for x in range(2)]
-  J[0][0] = dXdKsi;
-  J[0][1] = dYdKsi;
-  J[1][0] = dXdEta;
-  J[1][1] = dYdEta;
+  J[0][0] = dXdKsi
+  J[0][1] = dYdKsi
+  J[1][0] = dXdEta
+  J[1][1] = dYdEta
 
   detJ = (J[0][0] * J[1][1]) - (J[0][1] * J[1][0])
   inverseDetJ = 1/detJ
@@ -64,4 +68,21 @@ def jacobian(nOfElem, nOfIP, e42d: Element4_2D, grid: Grid):
       J[i][j] = inverseDetJ * transposeJ[i][j]
 
   printMatrix(J)
-  return J
+  return J, detJ
+
+def calcHMatrix(e42d: Element4_2D, J, detJ, kT):
+  ndN = 4
+  matrix_dNdX = [[0.0 for x in range(ndN)] for y in range(e42d.nPoints)] 
+  matrix_dNdY = [[0.0 for x in range(ndN)] for y in range(e42d.nPoints)]
+ 
+  for ip in range(e42d.nPoints):
+    for j in range(ndN):
+      matrix_dNdX[ip][j] = J[0][0] * e42d.matrix_dNdKsi[ip][j] + J[0][1] * e42d.matrix_dNdEta[ip][j]
+      matrix_dNdY[ip][j] = J[1][0] * e42d.matrix_dNdKsi[ip][j] + J[1][1] * e42d.matrix_dNdEta[ip][j]
+
+  H_ipN = [[0.0 for x in range(4)] for y in range(4)]
+  H_ip = [H_ipN] * e42d.nPoints 
+
+  for i in range(4):
+    for j in range(4):
+      H_ipN[i][j] = matrix_dNdX[i][j] * matrix_dNdX[i][j]
